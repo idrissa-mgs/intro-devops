8be921b9d05c98f22a03c9849341f3d861cc8aff
Xfive <xfive@aizen.home>
2025-01-04 16:20:49 +0200

    Calculatrice

---
+++ calculate.py
@@ -0,0 +1,12 @@
+nbr1 = int(input("Entrer le premier nombre : "))
+nbr2 = int(input("Entrer le deuxieme nombre : "))
+
+add = nbr1 + nbr2
+diff = nbr1 - nbr2
+mult = nbr1 * nbr2
+div = nbr1 / nbr2
+
+print(f"Addition {add}")
+print(f"Soustraction {diff}")
+print(f"ADivision {mult}")
+print(f"Division {div}")
