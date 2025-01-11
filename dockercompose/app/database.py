0f66f79f0fba781eaab6f13088a22c9e5a52619d
Idriss Magassa <idrissamagassa@gmail.com>
2025-01-11 16:30:49 +0100

    docker-compose

--- dockercompose/app/database.py
+++ dockercompose/app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os 
 
-DATABASE_URL = "postgresql://user:password@db:5432/mydatabase"
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
