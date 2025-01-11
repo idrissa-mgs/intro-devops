e7e0198c51209ddfa146069121dd7390a595df5b
Maramata DIOP <123824844+MARAMATA@users.noreply.github.com>
2025-01-11 13:44:41 +0000

    Update simple-api.py

--- dockerfile/simple-api.py
+++ dockerfile/simple-api.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
+# Récupérer la variable d'environnement
+GREETING_NAME = os.getenv("GREETING_NAME", "World")
 
 @app.get("/")
 def read_root():
-    return "Hello World! We are learning Docker"
+    return f"Hello {GREETING_NAME}! We are learning Docker"
